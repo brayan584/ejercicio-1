a84434c48665a911233825294c2716d7a24513e4
estudiante estudi. estudiante <estudiante@TALLERINF.COM>
2024-11-13 19:45:08 -0400

    comentario 2

--- ejercicio-git.python.py
+++ ejercicio-git.python.py
@@ -2,3 +2,11 @@
 print("Hola mundo Git")
 
 print('Hola mundo desde casa')
+
+cantidad = 0
+
+while True:
+    cantidad +=1
+    print(f'estoy cambiando la cantidad a: {cantidad}')
+    if cantidad == 10:
+        break
